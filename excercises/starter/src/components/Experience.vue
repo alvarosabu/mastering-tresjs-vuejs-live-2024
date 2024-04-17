690e308fa7a3f8d2cd3e8c69b41be79bc2e6cf06
alvarosabu <alvaro.saburido@gmail.com>
2024-04-17 11:07:35 +0200

    feat: excercises 0-2

--- excercises/starter/src/components/Experience.vue
+++ excercises/starter/src/components/Experience.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import { OrbitControls } from '@tresjs/cientos'
 </script>
 
 <template>
-  <TresPerspectiveCamera :position="[3, 3, 3]" />
-  <OrbitControls />
+  <TresPerspectiveCamera :position="[3, 3, 3]" :look-at="[0,0,0]" />
+  <TresAxesHelper />
   <TresGridHelper />
   <TresAmbientLight :intensity="1" />
 </template>
